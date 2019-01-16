871686ec5725b6e870b29b3e85843164405b3e4d
Quy Le <lnquy.it@gmail.com>
2019-01-16 16:48:01 +0700

    Get server port from Heroku env

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -9,14 +10,19 @@ import (
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world!"))
 	})
 
-	log.Infof("Main: server starting at 8080")
-	if err := http.ListenAndServe(":8080", r); err != nil && err != http.ErrServerClosed {
-		log.Panicf("Main: failed to start server on 8080: %v", err)
+	log.Infof("Main: server starting at %s", port)
+	if err := http.ListenAndServe(":" + port, r); err != nil && err != http.ErrServerClosed {
+		log.Panicf("Main: failed to start server on %s: %v", port, err)
 	}
 	log.Info("Main: exit")
 }
