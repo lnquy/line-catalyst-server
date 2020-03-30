0d532f47b21deb429a20d1abea868952a31e8cce
Quy Le <lnquy.it@gmail.com>
2020-03-30 22:51:49 +0700

    [master] Location

--- internal/model/schedule.go
+++ internal/model/schedule.go
@@ -24,7 +24,7 @@ func (s *Schedule) String() string {
 	if !s.IsDone {
 		cronSched, err := cron.ParseStandard(s.Cron)
 		if err == nil && cronSched != nil {
-			_, offset := time.Time{}.In(utils.GlobalLocation).Zone()
+			_, offset := time.Now().In(utils.GlobalLocation).Zone()
 			next = cronSched.Next(s.LastRun).Add(time.Second * time.Duration(-offset)).In(utils.GlobalLocation).Format(time.RFC3339)
 		}
 	}
@@ -36,7 +36,7 @@ func (s *Schedule) ShortString() string {
 	if !s.IsDone {
 		cronSched, err := cron.ParseStandard(s.Cron)
 		if err == nil && cronSched != nil {
-			_, offset := time.Time{}.In(utils.GlobalLocation).Zone()
+			_, offset := time.Now().In(utils.GlobalLocation).Zone()
 			next = cronSched.Next(s.LastRun).Add(time.Second * time.Duration(-offset)).In(utils.GlobalLocation).Format(time.RFC3339)
 		}
 	}
