fa4f9f6ddff0c64fc82a9585456c3ea6ffeb25cd
Quy Le <lnquy.it@gmail.com>
2020-03-30 22:54:20 +0700

    [master] Location

--- internal/model/schedule.go
+++ internal/model/schedule.go
@@ -24,8 +24,8 @@ func (s *Schedule) String() string {
 	if !s.IsDone {
 		cronSched, err := cron.ParseStandard(s.Cron)
 		if err == nil && cronSched != nil {
-			_, offset := time.Now().In(utils.GlobalLocation).Zone()
-			next = cronSched.Next(s.LastRun).Add(time.Second * time.Duration(-offset)).In(utils.GlobalLocation).Format(time.RFC3339)
+			// _, offset := time.Now().In(utils.GlobalLocation).Zone()
+			next = cronSched.Next(s.LastRun.In(utils.GlobalLocation)).In(utils.GlobalLocation).Format(time.RFC3339)
 		}
 	}
 	return fmt.Sprintf("Name: %s\nMessage: %s\nFinished: %v\nSchedule: %s\nLast run: %s\nNext run: %s", s.Name, s.Message, s.IsDone, s.Cron, s.LastRun.In(utils.GlobalLocation).Format(time.RFC3339), next)
@@ -36,8 +36,8 @@ func (s *Schedule) ShortString() string {
 	if !s.IsDone {
 		cronSched, err := cron.ParseStandard(s.Cron)
 		if err == nil && cronSched != nil {
-			_, offset := time.Now().In(utils.GlobalLocation).Zone()
-			next = cronSched.Next(s.LastRun).Add(time.Second * time.Duration(-offset)).In(utils.GlobalLocation).Format(time.RFC3339)
+			// _, offset := time.Now().In(utils.GlobalLocation).Zone()
+			next = cronSched.Next(s.LastRun.In(utils.GlobalLocation)).In(utils.GlobalLocation).Format(time.RFC3339)
 		}
 	}
 	return fmt.Sprintf("Sent by reminder: %s\nNext run: %s", s.Name, next)
